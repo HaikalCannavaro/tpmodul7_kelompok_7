4e05fab2383db6aa6945dbcaf3a30dd676d28a75
HaikalCannavaro <haikalcannavaro64@gmail.com>
2025-04-07 19:41:16 +0700

    menambahkan class KuliahMahasiswa103022300106

--- tpmodul7_kelompok_7/Program.cs
+++ tpmodul7_kelompok_7/Program.cs
@@ -4,7 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        DataMahasiswa103022300106 mhs = new DataMahasiswa103022300106();
-        mhs.ReadJSON();
+        //DataMahasiswa103022300106 mhs = new DataMahasiswa103022300106();
+        //mhs.ReadJSON();
+
+        KuliahMahasiswa103022300106 kuliah = new KuliahMahasiswa103022300106();
+        kuliah.ReadJSON();
     }
 }
